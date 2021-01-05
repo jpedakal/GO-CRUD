0d50f9454537803003220927bddfe50aa15d34ac
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-01-05 21:12:44 +0530

    update

--- app.go
+++ app.go
@@ -1,33 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"mongo"
 )
-
-type Trainer struct {
-	Name string
-	Age  int
-	City string
-}
-
 func main() {
-	
-
-	collection := client.Database("test").Collection("trainers")
-
-	jayakrishna := Trainer{"Jayakrishna", 27, "Bangalore"}
-
-	insertResult, err := collection.InsertOne(context.TODO(), jayakrishna)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Inserted document", insertResult.InsertedID)
-
+	d.Connect()
 }
+
--- database.go
+++ database.go
@@ -1,5 +1,16 @@
+package mongo
 
-func mogoConnect(){
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// This func must be Exported, Capitalized, and comment added.
+func Connect() {
 	URI := "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb"
 	clientOptions := options.Client().ApplyURI(URI)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
