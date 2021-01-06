294a437eede2129d520aecc7f3bb51d2c814f95d
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-01-06 21:01:27 +0530

    update

--- src/api/handler.go
+++ src/api/handler.go
@@ -1,9 +1,17 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 )
+
+type myData struct {
+	Name string   `json:"name"`
+	Age  int      `json:"age"`
+	City string   `json:"city"`
+}
 
 // GetData from database
 func GetData() {
@@ -11,8 +19,16 @@ func GetData() {
 }
 
 // PostData into database
-func PostData() {
+func PostData(rw http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
 
+	var data myData
+	err := decoder.Decode(&data)
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Println("data", data)
 }
 
 // HelloHandler to send data
--- src/index.go
+++ src/index.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 
+	http.HandleFunc("/insert", handler.PostData)
 	http.HandleFunc("/hello", handler.HelloHandler)
 	http.ListenAndServe(":8000", nil)
 }
